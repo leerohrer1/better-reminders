bbe87d401b011138f8e065034083233280896512
leerohrer1 <leerohrer1@gmail.com>
2022-04-29 12:33:51 -0500

    Getting readlineSync to work

--- index.mjs
+++ index.mjs
@@ -3,6 +3,9 @@ import Domain from './domain-class.mjs';
 import Occurence from './occurence-class.mjs';
 import readlineSync from 'readline-sync';
 
-console.log(`Welcome your personalized reminders program!
-You can start by looking at your growth domains or the reminders you have set.`);
+console.log(`Welcome to your personalized reminders program!
+
+You can start by looking at your growth domains or the reminders you have set.
+For domains, type "d" and hit enter. For reminders, type "r" and hit enter.
+`);
 let start = readlineSync.question(`Please enter where would you want to start:\n`);
